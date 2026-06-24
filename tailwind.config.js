92d6e7b13ccf82602b369753d3e227c46a8e3b5f
bishalgaming468-cell <bishalgaming468@gmail.com>
2026-06-24 13:46:36 +0545

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        nova: {
+          400: '#748ffc',
+          500: '#5c7cfa',
+          600: '#4c6ef5',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      animation: {
+        float: 'float 6s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
